e1100d5cf4d4a7a363a4c89c203962abe37fd6ee
Prateek <prateeknv99@gmail.com>
2017-10-17 23:11:05 +0530

    third commit

--- app/src/main/java/com/example/palak/palak/cardone.java
+++ app/src/main/java/com/example/palak/palak/cardone.java
@@ -1,7 +1,10 @@
 package com.example.palak.palak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class cardone extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class cardone extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardone);
+        Button button1 = (Button) findViewById(R.id.btn_n1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(cardone.this,cardtwo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
